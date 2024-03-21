273cf4f8d82c34faf13db9ee238c6260fe0e8d76
Jack Crouse <160173996+jacrouse@users.noreply.github.com>
2024-03-21 15:11:09 -0400

    Changed host again

--- traceroute.js
+++ traceroute.js
@@ -19,7 +19,7 @@ function traceroute(host)
 
     var $j = jQuery.noConflict();
 
-    let tracertServerHost = "http://45.55.104.184:5000/request";
+    let tracertServerHost = "http://167.99.0.220:5000/request";
 
     //get visitor IP
     $j.ajax({
